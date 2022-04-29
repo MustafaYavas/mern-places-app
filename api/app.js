eb5569589ee06d95e5f1be51e55d974e27092756
MustafaYavas <mustafayavas40@gmail.com>
2022-04-29 08:28:47 +0300

    Fix user route name and comments

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import placesRoutes from './routes/places-routes.js';  // it is kind of a middleware, so we can use it with app.use()
-// import usersRoutes from './routes/user-routes.js';
+// import usersRoutes from './routes/users-routes.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/api/places' ,placesRoutes);   // => localhost:3000/api/places/...
 // app.use('/api/users' ,usersRoutes);   // => localhost:3000/api/users/...
 
-
+// Triggered if there is an error in the routes
 app.use((error, req, res, next) => {
     if(res.headersSent) {
         return next(error)
--- api/routes/places-routes.js
+++ api/routes/places-routes.js
@@ -2,13 +2,13 @@ import express from 'express';
 
 import { getPlaceById, getPlaceByUserId, createPlace } from '../controllers/places-controllers.js';
 
-// Modüler, monte edilebilir route handler'lar oluşturmak için express.Router sınıfını kullanabiliriz.
+// Use the express.Router class to create modular, mountable route handlers
 const router = express.Router();
 
 
 // Routes
 router.get('/:pid', getPlaceById);
 router.get('/user/:uid', getPlaceByUserId);
-router.post('/', createPlace)
+router.post('/', createPlace);
 
 export default router;
